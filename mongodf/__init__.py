c9533d2e0f9547ba80a5cee8f2b2a11db81c8275
VK <viktor@krueckl.de>
2022-01-19 08:50:04 +0100

    init mongodf without db calls

--- mongodf/__init__.py
+++ mongodf/__init__.py
@@ -3,28 +3,31 @@ from .filter import Filter
 from .dataframe import DataFrame
 
 
-def from_mongo(mongo, database, collection):
+def from_mongo(mongo, database, collection, columns=None, filter={}):
 
     _db = mongo.get_database(database)
     _coll = _db.get_collection(collection)
 
-    # compute the colums of the data
-    _columns = list(_coll.aggregate([
-        {"$project": {
-            "data": {"$objectToArray": "$$ROOT"}
-        }},
-        {"$project": {"data": "$data.k"}},
-        {"$unwind": "$data"},
-        {"$group": {
-            "_id": None,
-            "keys": {"$addToSet": "$data"}
-        }}
-    ]))[0]["keys"]
+    if columns is None:
+        # compute the colums of the data
+        _columns = list(_coll.aggregate([
+            {"$project": {
+                "data": {"$objectToArray": "$$ROOT"}
+            }},
+            {"$project": {"data": "$data.k"}},
+            {"$unwind": "$data"},
+            {"$group": {
+                "_id": None,
+                "keys": {"$addToSet": "$data"}
+            }}
+        ]))[0]["keys"]
 
-    _columns = [c for c in _columns if c != "_id"]
+        _columns = [c for c in _columns if c != "_id"]
+    else:
+        _columns = columns
 
     mf = DataFrame(mongo, _db, _coll, _columns)
-    mf._filter = Filter(mf, {})
+    mf._filter = Filter(mf, filter)
     return mf
 
 
